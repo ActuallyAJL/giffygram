e7d61209ea0fd232d807c419ae6cf8c8e12a0f4c
ActuallyAJL <zero3260@hotmail.com>
2022-03-28 15:40:16 -0500

    caught up

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,32 +1,56 @@
 const userSrc = "http://localhost:8088/users";
-const postSrc = "http://localhost:8088/posts";
 
 export const getUsers = () => {
-
-    return fetch(userSrc)
-    .then(response => response.json())
-    .then(parsedResponse => {
-        // do something with response here
-        return parsedResponse;
-    })
-}
-
-export const getPosts = () => {
-
-    return fetch(postSrc)
-    .then(response => response.json())
-    .then(parsedResponse => {
-        // do something with response here
-        return parsedResponse;
-    })
-}
+  return fetch(userSrc)
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      // do something with response here
+      return parsedResponse;
+    });
+};
 
 const loggedInUser = {
-	id: 2,
-	name: "Alex Levy",
-	email: "alex.j.levy@me.com"
-}
+  id: 2,
+  name: "Alex Levy",
+  email: "alex.j.levy@me.com",
+};
 
 export const getLoggedInUser = () => {
-	return loggedInUser;
-}
+  return loggedInUser;
+};
+
+let postCollection = [];
+
+export const usePostCollection = () => {
+  //Best practice: we don't want to alter the original state, so
+  //make a copy of it and then return it
+  //The spread operator makes this quick work
+  return [...postCollection];
+};
+export const getPosts = () => {
+  return fetch("http://localhost:8088/posts")
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      postCollection = parsedResponse;
+      return parsedResponse;
+    });
+};
+
+export const createPost = (postObj) => {
+  return fetch("http://localhost:8088/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postObj),
+  }).then((response) => response.json());
+};
+
+export const deletePost = (postId) => {
+  return fetch(`http://localhost:8088/posts/${postId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.json());
+};
---
+++ src/scripts/feed/Post.js
@@ -0,0 +1,12 @@
+export const Post = (postObject) => {
+  return `
+    <section class="post">
+      <header>
+          <h2 class="post__title">${postObject.title}</h2>
+      </header>
+      <img class="post__image" src="${postObject.imageURL}" />
+      <div><button id="edit--${postObject.id}">Edit</button>
+      <button id="delete__${postObject.id}">Delete</button></div>
+    </section>
+  `
+}
